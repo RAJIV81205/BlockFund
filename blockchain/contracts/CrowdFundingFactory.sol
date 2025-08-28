488a5a69a44abee4f721fa5d005bc2e90677b0fe
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-28 21:32:50 +0530

    Remove hardhat dependency from package.json to streamline project dependencies.

---
+++ blockchain/contracts/CrowdFundingFactory.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {CrowdFunding} from "./CrowdFunding.sol";
+
+contract CrowdFundingFactory {
+    address public owner;
+    bool public paused;
+
+    struct Campaign {
+        address campaignAddress;
+        address owner;
+        string name;
+        uint256 creationTime;
+    }
+
+    Campaign[] public campaigns;
+
+    mapping(address => Campaign[]) public userCampaigns;
+
+    event CampaignCreated(address indexed campaignAddress, address indexed owner, string name, uint256 creationTime);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    modifier notPaused() {
+        require(!paused, " Factory is Paused");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createCampaign(
+        string memory _name,
+        string memory _description,
+        uint256 _goal,
+        uint256 _duration
+    ) external notPaused {
+        CrowdFunding newCampaign = new CrowdFunding(
+            msg.sender,
+            _name,
+            _description,
+            _goal,
+            _duration
+        );
+
+        address campaignAddress = address(newCampaign);
+
+        Campaign memory campaign = Campaign({
+            campaignAddress : campaignAddress ,
+            owner : msg.sender,
+            name : _name,
+            creationTime : block.timestamp
+
+
+        });
+
+        campaigns.push(campaign);
+        userCampaigns[msg.sender].push(campaign);
+        emit CampaignCreated(campaignAddress, msg.sender, _name, block.timestamp);
+    }
+
+    function getUserCampaigns(address _user) external view returns(Campaign[] memory){
+        return userCampaigns[_user];
+    } 
+
+    function getAllCampaigns() external view returns(Campaign[] memory){
+        return campaigns;
+    }
+
+     function togglePause() external onlyOwner {
+        paused = !paused;
+    }
+}
