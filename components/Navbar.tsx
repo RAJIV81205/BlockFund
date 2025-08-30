b512047853a364b4f315867e5dacfc8665e6612f
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-30 11:12:22 +0530

    Refactor: Replace Next.js Image with SVG for Ethereum logo
    Replaced the `next/image` component with an inline SVG for the Ethereum logo in the Navbar. This removes the dependency on `next/image` for this icon and slightly adjusts the account display padding for better visual alignment.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useWeb3 } from "@/contexts/Web3Context";
-import Image from "next/image";
+
 
 export default function Navbar() {
   const { account, isConnecting, connectWallet, isConnected, balance } = useWeb3();
@@ -32,16 +32,11 @@ export default function Navbar() {
           </div>
 
           {/* Account/Connect Section */}
-          <div>
+          <div className="h-3/4">
             {isConnected ? (
-              <div className="flex items-center space-x-3 border border-gray-300 rounded-lg px-4 py-2">
+              <div className="flex items-center space-x-3 border border-gray-300 rounded-lg px-4 py-1.5">
                 {/* Avatar */}
-                <Image
-                  src="/Ethereum-logo.webp"
-                  alt="Ethereum logo"
-                  width={20}
-                  height={20}
-                />
+                <svg width="20" height="30" viewBox="0 0 256 417" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill="#343434" d="M127.961 0l-2.795 9.5v275.668l2.795 2.79 127.962-75.638z" /><path fill="#8C8C8C" d="M127.962 0L0 212.32l127.962 75.639V154.158z" /><path fill="#3C3C3B" d="M127.961 312.187l-1.575 1.92v98.199l1.575 4.6L256 236.587z" /><path fill="#8C8C8C" d="M127.962 416.905v-104.72L0 236.585z" /><path fill="#141414" d="M127.961 287.958l127.96-75.637-127.96-58.162z" /><path fill="#393939" d="M0 212.32l127.96 75.638v-133.8z" /></svg>
                 {/* Account Info */}
                 <div className="flex flex-col">
                   <span className="text-sm font-mono text-gray-900">
